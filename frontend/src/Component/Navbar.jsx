29881ddd61a498578b0a2bcdd33afd6176fb7fa4
Rishabh Jain <jainrishabh2610@gmail.com>
2025-04-26 17:25:11 +0530

    logo

--- frontend/src/Component/Navbar.jsx
+++ frontend/src/Component/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import airbnb
+import airbnblogo1 from '../assets/airbnblogo1.png' 
 const Navbar = () => {
   return (
     <div>
-      <div className='w-screen min-h-[80px]  border-b-[1px] border-black'>
+      <div className='w-screen min-h-[80px]  border-b-[1px] border-black flex items-center justify-between'>
         <div>
-          <img src=""/>
+          <img src={airbnblogo1} className='h-22 w-64 px-12 py-4'/>
         </div>
+        <div>
+        <input type="text"/>
+        <button>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-search-icon lucide-search"><circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/></svg>
+        </button>
+        </div>
+
       </div>
     </div>
   )
