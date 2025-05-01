66b38f4179832a86a0d43b345bcf8473015fbaf9
Rishabh Jain <jainrishabh2610@gmail.com>
2025-05-01 09:52:57 +0530

    Update user.controller.js

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -4,10 +4,10 @@ const getCurrentUser=async(req,res)=>{
   try{
     let user=await User.findById(req.userId).select(-password);
     if(!user){
-      return res.status(404).json({message:"User not found"})
+      return res.status(404).json({message:"User not found"});
     }
     else{
-      return res.status(200).json({message:"User found",user})
+      return res.status(200).json({message:"User found",user});
     }
   }catch(error){
      return res.status(500).json({message:"Error in getting current user",error:error.message})
