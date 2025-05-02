df5fea4f65226109f8273f9fb505c69159e362b2
Rishabh Jain <jainrishabh2610@gmail.com>
2025-05-03 01:17:39 +0530

    Update user.model.js

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,26 +1,29 @@
-const mongoose=require('mongoose');
-const {Schema,model}=mongoose;
-const userSchema=new Schema({
-  name:{
-    type:String,
-    required:true
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    listing: {
+      type: Schema.Types.ObjectId,
+      ref: "Listing",
+    },
+    booking: {
+      type: Schema.Types.ObjectId,
+      ref: "Booking",
+    },
   },
-  email:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  password:{
-    type:String,
-    required:true
-  },
-  listing:{
-    type:Schema.Types.ObjectId,
-    ref:'Listing'
-  },
-  booking:{
-    type:Schema.Types.ObjectId,
-    ref:'Booking'
-  }
-},{timestamps:true});
-module.exports=model('User',userSchema);
+  { timestamps: true }
+);
+module.exports = model("User", userSchema);
