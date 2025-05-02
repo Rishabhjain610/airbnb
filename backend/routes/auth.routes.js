d67ceadba792f62a25a35cc079a1bc7f3431bd23
Rishabh Jain <jainrishabh2610@gmail.com>
2025-05-03 01:17:24 +0530

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,8 @@
-const express=require('express');
-const authRouter=express.Router();
-const {signUp,login,logout}=require('../controller/auth.controller.js');
-authRouter.post('/signup',signUp);
-authRouter.post('/login',login);
-authRouter.post('/logout',logout);
+const express = require("express");
+const authRouter = express.Router();
+const { signUp, login, logout } = require("../controller/auth.controller.js");
+authRouter.post("/signup", signUp);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
-
-
-
-
-module.exports=authRouter;
+module.exports = authRouter;
