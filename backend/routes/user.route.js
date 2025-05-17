1c462bc9a859b860318af23a8cf27dd19fc88564
Rishabh Jain <jainrishabh2610@gmail.com>
2025-05-17 15:50:46 +0530

    Update user.route.js

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,4 +2,5 @@ const express = require("express");
 const { authCheck } = require("../middleware/auth.middleware");
 let userRouter = express.Router();
 userRouter.get("currentuser", authCheck);
+
 module.exports = userRouter;
