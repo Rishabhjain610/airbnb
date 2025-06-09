d440eee8a643e796d32cfb5680ab21a53d61b557
Rishabh Jain <jainrishabh2610@gmail.com>
2025-06-10 01:37:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend's URL
+    origin: "https://airbnb-7qyl.onrender.com", // Replace with your frontend's URL
     credentials: true, // Allow cookies to be sent
 
     //this is needed when res.cookie is used in the backend
